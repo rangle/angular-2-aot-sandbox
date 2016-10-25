608a27fdc721212f5626593e5504c54dbe5bee0b
Mark Hayes <mark.hayes@rangle.io>
2016-10-25 15:02:54 -0400

    Move tests into directory and retrieve directory listing for available test cases

--- sandbox-loader.js
+++ sandbox-loader.js
@@ -1,76 +1,66 @@
 'use strict';
 
-const fs = require("fs");
-const ncp = require("ncp");
+const fs = require('fs');
+const ncp = require('ncp');
 const spawn = require('child_process').spawn;
 const spawnSync = require('child_process').spawnSync;
-const whichCase = process.argv[2] || "NULL";
+const whichCase = process.argv[2] || 'NULL';
 const aot = process.argv[3] || true;
-const avaliableCases = [
-  "control",
-  "default-exports",
-  "form-control",
-  "form-control-error",
-  "func-in-providers",
-  "func-in-declarations",
-  "func-in-routes",
-  "private-property",
-  "private-input",
-  "private-output",
-  "private-viewchild",
-  "private-contentchild",
-  "private-hostbinding",
-  "interpolated-es6",
-  "template-variable",
-  "template-expression",
-  "nomut-property-decorator",
-  "mut-property-decorator",
-  "service-with-generic-type-param",
-  "ng2-redux-@select",
-];
+const path = require('path');
+const availableCases = getAvailableCases(path.resolve(__dirname, 'tests'));
 
-if (whichCase === "total") {
-   for (var i = 0; i < avaliableCases.length; i++) {
-     console.log("Start", "["+avaliableCases[i]+"]", "case:");
-     let testCase = spawnSync('node', ['sandbox-loader.js', avaliableCases[i]]);
-     console.log(testCase.stderr.toString('utf8'));
-     console.log(testCase.stdout.toString('utf8'));
-   }
-   process.exit(0);
+if (whichCase === 'total') {
+  for (let i = 0; i < availableCases.length; i++) {
+    console.log('Start', '[' + availableCases[i] + ']', 'case:');
+    let testCase = spawnSync('node', ['sandbox-loader.js', availableCases[i]]);
+    console.log(testCase.stderr.toString('utf8'));
+    console.log(testCase.stdout.toString('utf8'));
+  }
+  process.exit(0);
 }
 
-if (avaliableCases.indexOf(whichCase) === -1) {
-  console.log("["+whichCase+"]", "case is not avaliable");
+if (availableCases.indexOf(whichCase) === -1) {
+  console.log('[' + whichCase + ']', 'case is not available');
   process.exit(1);
 }
 
 const clean = spawn('npm', ['run', 'clean']);
-clean.on('close', function(code){
+clean.on('close', function (code) {
   if (code === 0) {
-    ncp(whichCase, "sandbox", function(error){
+    ncp(path.resolve(__dirname, 'tests', whichCase), 'sandbox', function (error) {
       if (error) {
         console.log(error);
         process.exit(1);
       }
-      fs.writeFileSync("./sandbox/tsconfig.json", fs.readFileSync("tsconfig.json"));
-      console.log("AoT:", aot);
-      console.log("Test case", "["+whichCase+"]", "LOADED");
-      let commandOptions =  ['run', 'sandbox']
+      fs.writeFileSync('./sandbox/tsconfig.json', fs.readFileSync('tsconfig.json'));
+      console.log('AoT:', aot);
+      console.log('Test case', '[' + whichCase + ']', 'LOADED');
+      let commandOptions = ['run', 'sandbox'];
       if (aot !== true) {
         commandOptions = ['run', 'sandbox:jit']
       }
-      console.log("Test case", "["+whichCase+"]", "BUILDING");
+      console.log('Test case', '[' + whichCase + ']', 'BUILDING');
       const sandbox = spawn('npm', commandOptions);
       sandbox.stderr.on('data', (data) => {
         console.log(`stderr: ${data}`);
       });
       sandbox.on('close', (code) => {
         if (code === 0) {
-          console.log("Test case", "["+whichCase+"]", "PASS");
+          console.log('Test case', '[' + whichCase + ']', 'PASS');
         } else {
-          console.log("Test case", "["+whichCase+"]", "FAILED");
+          console.log('Test case', '[' + whichCase + ']', 'FAILED');
         }
       });
     })
   }
-})
+});
+
+function getAvailableCases(testsPath) {
+  const files = fs.readdirSync(testsPath, {});
+  const isDirectory = function(testPath) {
+    const filePath = path.resolve(testsPath, testPath);
+    const fileStat = fs.lstatSync(filePath);
+    return fileStat.isDirectory();
+  };
+  return files.filter(isDirectory);
+}
